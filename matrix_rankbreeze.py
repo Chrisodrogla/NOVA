fba0e9cd4cce2ec68560312c54a3373bd24b8869
Christian Algordo <123637289+Chrisodrogla@users.noreply.github.com>
2024-05-11 08:31:56 +0800

    Update matrix_rankbreeze.py

--- matrix_rankbreeze.py
+++ matrix_rankbreeze.py
@@ -1,321 +1,71 @@
 import os
-import time
-from selenium import webdriver
-import shutil
-from selenium.common.exceptions import NoSuchElementException
-import datetime
-import pytz
-import pandas as pd
-from google.oauth2.service_account import Credentials
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-import json
+import pyodbc
 
-# Google Sheets setup
-SHEET_ID = '1RG-5uy_k3GbpDYINKDAZLh0UomU3U41N-Pk50Qtaus8'
-SHEET_NAME = 'Data'
-SHEET_NAME2 = 'impressions'
-SHEET_NAME3 = 'click'
-SHEET_NAME4 = 'listing_v'
-SHEET_NAME5 = 'conversion_r'
-SHEET_NAME6 = 'lead_times'
-SHEET_NAME7 = 'abnb_occu'
-SHEET_NAME8 = 'daily_rates'
-SHEET_NAME9 = 'revenue'
+# Azure SQL Database connection string
+connection_string = os.environ.get('SECRET_CHRISTIANSQL_STRING')
 
-# Get Google Sheets credentials from environment variable
-GSHEET_CRED_MATRIX_RBREEZE = os.getenv("GSHEET_CRED_MATRIX_RBREEZE")
-credentials = Credentials.from_service_account_info(json.loads(GSHEET_CRED_MATRIX_RBREEZE))
+# Establish connection
+conn = pyodbc.connect(connection_string)
+cursor = conn.cursor()
 
-# Create Google Sheets API service
-service = build("sheets", "v4", credentials=credentials)
-
-eastern_tz = pytz.timezone("America/New_York")
-current_time = datetime.datetime.now(eastern_tz)
-
-username = os.environ['D_USERNAME_SECRET']
-passw = os.environ['D_PASSWORD_SECRET']
-
-website = "https://app.rankbreeze.com/listings"
-
-# Set up Chrome WebDriver
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--no-sandbox")
-
-driver = webdriver.Chrome(options=options)
-driver.get(website)
-
-time.sleep(2)
-
-driver.find_element("xpath", """(//div[@class="form-group"]/input)[1]""").send_keys(username)
-time.sleep(2)
-driver.find_element("xpath", """(//div[@class="form-group"]/input)[2]""").send_keys(passw)
-log = driver.find_element("xpath", """(//div[@class="form-group"]/input)[3]""")
-time.sleep(2)
-log.click()
-time.sleep(4)  # 20
-
-proxy_links = ["https://app.rankbreeze.com/rankings/71585",
-#     "https://app.rankbreeze.com/rankings/71572",
-    "https://app.rankbreeze.com/rankings/71571",
-    "https://app.rankbreeze.com/rankings/71570",
-#     "https://app.rankbreeze.com/rankings/71569",
-    "https://app.rankbreeze.com/rankings/71568",
-    "https://app.rankbreeze.com/rankings/71567",
-    "https://app.rankbreeze.com/rankings/71363",
-    "https://app.rankbreeze.com/rankings/71362",
-    "https://app.rankbreeze.com/rankings/71361",
-    "https://app.rankbreeze.com/rankings/71360",
-    "https://app.rankbreeze.com/rankings/71359",
-    "https://app.rankbreeze.com/rankings/71358",
-    "https://app.rankbreeze.com/rankings/70897",
-    "https://app.rankbreeze.com/rankings/70895",
-    "https://app.rankbreeze.com/rankings/70894",
-    "https://app.rankbreeze.com/rankings/70893",
-    "https://app.rankbreeze.com/rankings/70892",
-    "https://app.rankbreeze.com/rankings/70891",
-    "https://app.rankbreeze.com/rankings/70890",
-    "https://app.rankbreeze.com/rankings/70889",
-    "https://app.rankbreeze.com/rankings/70888",
-    "https://app.rankbreeze.com/rankings/70887",
-    "https://app.rankbreeze.com/rankings/70886",
-    "https://app.rankbreeze.com/rankings/70885",
-    "https://app.rankbreeze.com/rankings/70884",
-    "https://app.rankbreeze.com/rankings/70883",
-    "https://app.rankbreeze.com/rankings/70882",
-    "https://app.rankbreeze.com/rankings/70881",
-    "https://app.rankbreeze.com/rankings/70862",
-    "https://app.rankbreeze.com/rankings/70861",
-    "https://app.rankbreeze.com/rankings/70860",
-    "https://app.rankbreeze.com/rankings/70859",
-    "https://app.rankbreeze.com/rankings/70858",
-    "https://app.rankbreeze.com/rankings/70852",
-    "https://app.rankbreeze.com/rankings/70851",
-    "https://app.rankbreeze.com/rankings/70515",
-    "https://app.rankbreeze.com/rankings/70479",
-    "https://app.rankbreeze.com/rankings/70476",
-    "https://app.rankbreeze.com/rankings/70475",
-    "https://app.rankbreeze.com/rankings/70474",
-    "https://app.rankbreeze.com/rankings/70473",
-    "https://app.rankbreeze.com/rankings/70472",
-    "https://app.rankbreeze.com/rankings/70471",
-    "https://app.rankbreeze.com/rankings/70470",
-    "https://app.rankbreeze.com/rankings/70468",
-    "https://app.rankbreeze.com/rankings/70467",
-    "https://app.rankbreeze.com/rankings/70466",
-    "https://app.rankbreeze.com/rankings/70465",
-    "https://app.rankbreeze.com/rankings/70464",
-    "https://app.rankbreeze.com/rankings/70463",
-    "https://app.rankbreeze.com/rankings/70462",
-    "https://app.rankbreeze.com/rankings/70461",
-    "https://app.rankbreeze.com/rankings/70460",
-    "https://app.rankbreeze.com/rankings/70459",
-    "https://app.rankbreeze.com/rankings/70458",
-    "https://app.rankbreeze.com/rankings/70457",
-    "https://app.rankbreeze.com/rankings/70456",
-    "https://app.rankbreeze.com/rankings/70455", ]
-
-data = []
-overall_impressions = []
-overall_search_conversion_rate = []
-overall_listing_views = []
-overall_conversion_rate = []
-overall_lead_times = []
-overall_airbnb_occupancy = []
-overall_avg_daily_rates = []
-overall_revenue = []
-
-for website in proxy_links:
-    driver.get(website)
-    time.sleep(4)
-
-    driver.get(website)
-
-    time.sleep(3)
-
-    link = website
-
-    link_Id = website.strip("https://app.rankbreeze.com/rankings/")
-
-    proxy_title = driver.find_element("xpath", """//*[@id="get-email"]/div/main/div[3]/div[1]/h2""").get_attribute(
-        "innerText")
-
-    guest_satisfaction = driver.find_element("xpath", """(//div[@class="single-value"]/b)[1]""").get_attribute(
-        "innerText")
-
-    reviews_count = driver.find_element("xpath", """(//div[@class="single-value"]/b)[2]""").get_attribute("innerText")
-
-    date_str = current_time.strftime("%Y-%m-%d")  # 'YYYY-MM-DD'
-
-    date_hours_str = current_time.strftime("%Y-%m-%d %H:%M:%S")  # 'YYYY-MM-DD HH:MM:SS
+# Data to insert
+data = [{'Link': 'https',
+  'Link Id': '70',
+  'Rental Name': 'Wind',
+  'Reviews Count': '8.0',
+  'Star Reviews': '4',
+  'Date Gathered': '2024-05-10',
+  'Date Gathered Hours': '2024-05-10 17:41:52'},
+ {'Link': 'htt',
+  'Link Id': '70455',
+  'Rental Name': 'Stak',
+  'Reviews Count': '8.0',
+  'Star Reviews': '2',
+  'Date Gathered': '2024-05-10',
+  'Date Gathered Hours': '2024-05-10 17:41:52'}]  # Your data
+overall_impressions = [('December, 2023',
+  '0',
+  '1,835',
+  'http6',
+  '76',
+  'Wiey',
+  '2024-05-10',
+  '2024-05-10 17:41:52'),
+ ('January, 2024',
+  '0',
+  '2,365',
+  'htts:,
+  '70456',
+  'WindHiey',
+  '2024-05-10',
+  '2024-05-10 17:41:52')]  # Your overall_impressions data
 
 
-    # Define the function to extract table data
-    def extract_table_data(rows, num_columns):
-        table_data = []
-        for row in rows:
-            columns = row.find_elements("xpath", "./td")
-            if len(columns) >= num_columns:
-                column_texts = [column.get_attribute("innerText").strip() for column in columns[:num_columns]]
+insert_query1 = os.environ.get('INSERT_QUERY_1')
+insert_query2 = os.environ.get('INSERT_QUERY_2')
 
-                if constant_values:
-                    column_texts.extend(constant_values)
+# Define insert queries
 
-                table_data.append(tuple(column_texts))
-        return table_data
+# Insert data into table1
+for item in data:
+    cursor.execute(insert_query1, (item['Link'], item['Link Id'], item['Rental Name'], item['Star Reviews'], item['Reviews Count'], item['Date Gathered'], item['Date Gathered Hours']))
 
 
-    # Find and extract data for impressions and search conversion rate
-    impressions = driver.find_elements("xpath", """//*[@id="first_page_search_impressions"]//tbody/tr""")
-    search_conversion_rate = driver.find_elements("xpath", """//*[@id="search_conversion_rate"]//tbody/tr""")
 
-    listing_views = driver.find_elements("xpath", """//*[@id="page_views"]//tbody/tr""")
-    conversion_rate = driver.find_elements("xpath", """//*[@id="conversion_rate"]//tbody/tr""")
-    lead_times = driver.find_elements("xpath", """//*[@id="booking_lead_time"]//tbody/tr""")
-    airbnb_occupancy = driver.find_elements("xpath", """//*[@id="occupancy_rate"]//tbody/tr""")
-    avg_daily_rates = driver.find_elements("xpath", """//*[@id="average_daily_rate"]//tbody/tr""")
-    revenue = driver.find_elements("xpath", """//*[@id="revenue"]//tbody/tr""")
+for item in overall_impressions:
+    period = item[0]
+    impressions = item[1]
+    similar_listing = item[2]
+    link = item[3]
+    link_id = item[4]
+    rental_title = item[5]
+    date_data_gathered = item[6]
+    time_data_gathered = item[7]
+    cursor.execute(insert_query_overall, (period, impressions, similar_listing, link, link_id, rental_title, date_data_gathered, time_data_gathered))
 
-    constant_values = [link, link_Id, proxy_title, date_str, date_hours_str]
 
-    # Store extracted data in respective overall lists
-    overall_impressions.extend(extract_table_data(impressions, 3))
-    overall_search_conversion_rate.extend(extract_table_data(search_conversion_rate, 3))
-    overall_listing_views.extend(extract_table_data(listing_views, 3))
-    overall_conversion_rate.extend(extract_table_data(conversion_rate, 3))
-    overall_lead_times.extend(extract_table_data(lead_times, 3))
-    overall_airbnb_occupancy.extend(extract_table_data(airbnb_occupancy, 3))
-    overall_avg_daily_rates.extend(extract_table_data(avg_daily_rates, 3))
-    overall_revenue.extend(extract_table_data(revenue, 2))
+# Commit changes
+conn.commit()
 
-    data.append({
-        "Link": link,
-        "Link Id": link_Id,
-        "Rental Name": proxy_title,
-        "Reviews Count": guest_satisfaction,
-        "Star Reviews": reviews_count,
-        "Date Gathered": date_str,
-        "Date Gathered Hours": date_hours_str,
-    })
-
-# Append `data` to 'Data' sheet
-try:
-    # Convert `data` to a list of lists for Google Sheets
-    values = [list(d.values()) for d in data]
-
-    service.spreadsheets().values().append(
-        spreadsheetId=SHEET_ID,
-        range=SHEET_NAME,  # 'Data' sheet
-        valueInputOption="RAW",
-        body={"values": values},
-    ).execute()
-except HttpError as e:
-    print("Error appending data to 'Data' sheet in Google Sheets:", e)
-
-# Append `overall_impressions` to 'impressions' sheet
-try:
-    # Convert `overall_impressions` to a list of lists
-    values2 = list(overall_impressions)
-
-    service.spreadsheets().values().append(
-        spreadsheetId=SHEET_ID,
-        range=SHEET_NAME2,  # 'impressions' sheet
-        valueInputOption="RAW",
-        body={"values": values2},
-    ).execute()
-except HttpError as e:
-    print("Error appending data to 'impressions' sheet in Google Sheets:", e)
-
-try:
-
-    values3 = list(overall_search_conversion_rate)
-
-    service.spreadsheets().values().append(
-        spreadsheetId=SHEET_ID,
-        range=SHEET_NAME3,  # 'impressions' sheet
-        valueInputOption="RAW",
-        body={"values": values3},
-    ).execute()
-except HttpError as e:
-    print("Error appending data to 'click' sheet in Google Sheets:", e)
-
-try:
-
-    values4 = list(overall_listing_views)
-
-    service.spreadsheets().values().append(
-        spreadsheetId=SHEET_ID,
-        range=SHEET_NAME4,  # 'impressions' sheet
-        valueInputOption="RAW",
-        body={"values": values4},
-    ).execute()
-except HttpError as e:
-    print("Error appending data to 'listing_v' sheet in Google Sheets:", e)
-
-try:
-
-    values5 = list(overall_conversion_rate)
-
-    service.spreadsheets().values().append(
-        spreadsheetId=SHEET_ID,
-        range=SHEET_NAME5,  # 'impressions' sheet
-        valueInputOption="RAW",
-        body={"values": values5},
-    ).execute()
-except HttpError as e:
-    print("Error appending data to 'conversion_r' sheet in Google Sheets:", e)
-
-try:
-
-    values6 = list(overall_lead_times)
-
-    service.spreadsheets().values().append(
-        spreadsheetId=SHEET_ID,
-        range=SHEET_NAME6,  # 'impressions' sheet
-        valueInputOption="RAW",
-        body={"values": values6},
-    ).execute()
-except HttpError as e:
-    print("Error appending data to 'lead_times' sheet in Google Sheets:", e)
-
-try:
-
-    values7 = list(overall_airbnb_occupancy)
-
-    service.spreadsheets().values().append(
-        spreadsheetId=SHEET_ID,
-        range=SHEET_NAME7,  # 'impressions' sheet
-        valueInputOption="RAW",
-        body={"values": values7},
-    ).execute()
-except HttpError as e:
-    print("Error appending data to 'abnb_occu' sheet in Google Sheets:", e)
-
-try:
-
-    values8 = list(overall_avg_daily_rates)
-
-    service.spreadsheets().values().append(
-        spreadsheetId=SHEET_ID,
-        range=SHEET_NAME8,  # 'impressions' sheet
-        valueInputOption="RAW",
-        body={"values": values8},
-    ).execute()
-except HttpError as e:
-    print("Error appending data to 'daily_rates' sheet in Google Sheets:", e)
-
-try:
-
-    values9 = list(overall_revenue)
-
-    service.spreadsheets().values().append(
-        spreadsheetId=SHEET_ID,
-        range=SHEET_NAME9,  # 'impressions' sheet
-        valueInputOption="RAW",
-        body={"values": values9},
-    ).execute()
-except HttpError as e:
-    print("Error appending data to 'revenue' sheet in Google Sheets:", e)
-
+# Close connection
+conn.close()
