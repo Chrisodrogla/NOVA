64445a8406cb167e2689c999f6d99dde9bcd006d
Christian Algordo <123637289+Chrisodrogla@users.noreply.github.com>
2024-06-05 00:15:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ website = "https://app.rankbreeze.com/listings?page=13"
 
 # Set up Chrome WebDriver
 options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-gpu")
