19f7deb18fdfc97e75551104a9b25885d2046cb1
Christian <c.algordo@yahoo.com>
2024-06-19 05:19:11 +0800

    commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import json
 
 # Set up Chrome WebDriver with custom options
 options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 
