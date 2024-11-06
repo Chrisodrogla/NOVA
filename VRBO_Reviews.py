dddd680d0da0c7871d08a02d14276282409d6e23
Christian Algordo <123637289+Chrisodrogla@users.noreply.github.com>
2024-11-07 02:18:59 +0800

    Update VRBO_Reviews.py

--- VRBO_Reviews.py
+++ VRBO_Reviews.py
@@ -19,7 +19,7 @@ import json
 
 # Set up Chrome WebDriver with options
 options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
